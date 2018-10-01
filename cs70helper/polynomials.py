e5d828e45a50006a25eeb0f43fcee79eb5d496aa
alexander-zw <alexanderwu68@yahoo.com>
2018-10-01 10:53:21 -0700

    Fix bug in degree() that leading zeros mess it up

--- cs70helper/polynomials.py
+++ cs70helper/polynomials.py
@@ -3,20 +3,16 @@
 import sympy as sp
 from rsa import ModExp
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
 class Polynomial:
     def __init__(self, coeffs, n):
         # beginning at 0 degree term to d+1 degree term
+        # zero polynomial can be represented as empty list
         self.coeffs = coeffs
         self.n = n
 
     def of(self, x):
         y = 0
-        for i in range(len(self.coeffs)):
+        for i in range(self.degree + 1):
             y += (self.coeffs[i] * ModExp.power_mod(x, i, self.n)) % self.n
             y %= self.n
         return y
@@ -39,13 +35,13 @@ class Polynomial:
             sum_coeffs = other.coeffs.copy()
             poly_to_add = self
 
-        for i in range(len(self.coeffs)):
+        for i in range(self.degree + 1):
             sum_coeffs[i] = (sum_coeffs[i] + poly_to_add.coeff(i)) % self.n
         return Polynomial(sum_coeffs, self.n)
 
     def scale(self, a):
         scaled_coeffs = self.coeffs.copy()
-        for i in range(len(self.coeffs)):
+        for i in range(self.degree + 1):
             scaled_coeffs[i] = a * scaled_coeffs[i] % self.n
         return Polynomial(scaled_coeffs, self.n)
 
@@ -55,37 +51,44 @@ class Polynomial:
             raise ValueError(
                     "Cannot multiply polynomials of different finite fields")
 
-        prod_coeffs = [0] * (len(self.coeffs) * len(other.coeffs))
+        # zero polynomial is empty list
+        prod_coeffs = [0] * ((self.degree + 1 + other.degree + 1))
 
-        for i in range(len(self.coeffs)):
-            for j in range(len(other.coeffs)):
+        for i in range(self.degree + 1):
+            for j in range(other.degree + 1):
                 prod_coeffs[i + j] += (self.coeff(i) * other.coeff(j))\
                                         % self.n
                 prod_coeffs[i + j] %= self.n
         return Polynomial(prod_coeffs, self.n)
 
     # degree determined by number of points
+    # a point is a 2-tuple
     def lagrange_construct(points, n):
         result = Polynomial([0], n)
         for i in range(len(points)):
             p = points[i]
-            if p.y == 0:
+            if p[1] == 0:
                 # don't need to compute this delta
                 continue
             numerator = Polynomial([1], n) # start with 1
             for pp in points:
-                if p.x == pp.x:
+                if p[0] == pp[0]:
                     continue # skip current point
                 # I'm using negative coefficients here, but I think that's ok
-                numerator = numerator.mul(Polynomial([-pp.x, 1], n))
-            denominator = numerator.of(p.x)
+                numerator = numerator.mul(Polynomial([-pp[0], 1], n))
+            denominator = numerator.of(p[0])
             delta = numerator.scale(sp.mod_inverse(denominator, n))
-            result = result.add(delta.scale(p.y))
+            result = result.add(delta.scale(p[1]))
         return result
 
-    @property
+    @property # also trims leading zeros of polynomial
     def degree(self):
-        return len(self.coeffs) - 1
+        for i in range(len(self.coeffs) - 1, -1, -1):
+            if self.coeffs[i] != 0:
+                return i
+            self.coeffs.pop()
+        # we make the degree of the zero polynomial -1
+        return -1
 
     def __repr__(self):
         return "Polynomial({0}, {1})".format(self.coeffs, self.n)
@@ -105,6 +108,8 @@ class Polynomial:
 
     def __str__(self):
         string = "Polynomial: "
-        for i in range(len(self.coeffs) - 1, -1, -1):
+        if not self.coeffs: # zero polynomial
+            return string + "0"
+        for i in range(self.degree, -1, -1):
             string += Polynomial._term_str(i, self.coeffs[i])
         return string + " (mod {0})".format(self.n)
